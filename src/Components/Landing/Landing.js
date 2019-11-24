54440ce790c01d9947f34887a8092ba55017ba7e
noagalic <constanagnos@gmail.com>
2019-11-24 13:15:26 +0100

    landing/signup done

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -1,5 +1,18 @@
 import React from 'react';
 import "./Landing.css";
+import Particles from 'react-particles-js';
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 200,
+      density: {
+        enable: true,
+        value_area: 1000
+        }
+      }
+    }
+  }
 
 const doit = () => {
 	var style = document.createElement('style');
@@ -13,24 +26,25 @@ const doit = () => {
 const Landing = () => {
 	return (
 		<div>
-		<img class="logo" src={require("./FSB_Logo.png")} />
+		<Particles className='particles' class="particles" params={particlesOptions} />
+		<img alt="" class="logo" src={require("./FSB_Logo.png")} />
 		<main id="foo" className="pa4 black-80">
 			<div id="cont">
 			<form id="formy" onMouseEnter={setTimeout(doit, 1000)} className="measure center">
 			    <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-			      <legend className="f4 fw6 center">Sign In</legend>
+			      <legend className="f4 fw6 center">Prijava</legend>
 			      <div className="mt3">
 			        <label className="db fw6 lh-copy f6" for="email-address">Email</label>
 			        <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address" />
 			      </div>
 			      <div className="mv3">
-			        <label className="db fw6 lh-copy f6" for="password">Password</label>
+			        <label className="db fw6 lh-copy f6" for="password">Zaporka</label>
 			        <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password" />
 			      </div>
-			      <label className="pa0 ma0 lh-copy f6 pointer"><input type="checkbox" /> Remember me</label>
+			      <label className="pa0 ma0 lh-copy f6 pointer"><input type="checkbox" /> Zapamti me</label>
 			    </fieldset>
 			    <div className="">
-			      <input className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Sign in" />
+			      <input className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Prijavi me" />
 			    </div>
 			</form>
 			</div>
